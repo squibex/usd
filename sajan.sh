4f9f61afa1c2f506994378719b45a61d837be83e
squibex <90225595+squibex@users.noreply.github.com>
2021-09-09 21:23:23 +0700

    Update sajan.sh

--- sajan.sh
+++ sajan.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/squibex/prem/raw/main/lagan.sh
+wget https://github.com/squibex/usd/raw/main/lagan.sh
 wget https://github.com/squibex/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
